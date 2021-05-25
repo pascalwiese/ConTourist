d845836f4077a77a987c985211b8fc0d2ae6524e
TwoWaySix <2wei6@tuta.io>
2021-05-25 15:20:55 +0200

    debugging works. but recusive walking does not seem to be the best way

--- contourist/dissolving.py
+++ contourist/dissolving.py
@@ -11,6 +11,7 @@ def dissolve_all_contour_polygons(element_contour_polygons_dict, mesh, params, d
     print("\nDissolving all contour polygons...")
 
     debug_points = []
+    debug_diss_points = []
     dissolve_polygons_dict = {}
     for c_lo, c_hi in zip(params['contours'][:-1], params['contours'][1:]):
         c_key = "{}-{}".format(c_lo, c_hi)
@@ -25,7 +26,7 @@ def dissolve_all_contour_polygons(element_contour_polygons_dict, mesh, params, d
 
         for eid in range(len(is_elmt_dissolved)):
             # Skip if element was dissolved
-            if is_elmt_dissolved[eid] == True:
+            if is_elmt_dissolved[eid]:
                 continue
 
             # Skip if element has no contour polygon 
@@ -33,18 +34,21 @@ def dissolve_all_contour_polygons(element_contour_polygons_dict, mesh, params, d
                 is_elmt_dissolved[eid] = True
                 continue
             
-            polygon = [element_to_point(mesh.elements[eid], mesh.nodes)]
+            diss_polygon = [element_to_point(mesh.elements[eid], mesh.nodes)]
+            polygon = []
             is_elmt_dissolved[0] = True
             polygon = recursive_dissolving(polygon, eid, element_contour_polygons, is_elmt_dissolved, c_key, mesh, do_plot=do_plot)
 
             debug_points.append(polygon)
+            # debug_diss_points.append(diss_polygon)
             # fout = open("./testdata/debug2.txt", "w")
             # fout.write("id x y\n")
             # for i, pnt in enumerate(polygon):
             #     fout.write("{} {} {}\n".format(i, pnt[0], pnt[1]))
             # print(len(polygon))
             # a = input("Input needed.")
-    write_debug_points_to_shape(debug_points)
+    write_debug_points_to_shape(debug_points, "debug")
+    # write_debug_points_to_shape(debug_diss_points, "diss")
 
 
 def recursive_dissolving(polygon, eid, element_contour_polygons, is_elmt_dissolved, c_key, mesh, do_plot=False):
@@ -90,7 +94,8 @@ def recursive_dissolving(polygon, eid, element_contour_polygons, is_elmt_dissolv
 
     # First do dissolving
     for neid in dissolve_neighbour_eids:
-        polygon.append(element_to_point(mesh.elements[neid], mesh.nodes))
+        # diss_polygon.append(element_to_point(mesh.elements[neid], mesh.nodes))
+        # polygon.append(element_to_point(mesh.elements[neid], mesh.nodes))
         is_elmt_dissolved[neid] = True
 
     # Then dissolve the neighbours neighbours
@@ -106,7 +111,8 @@ def element_to_point(element, nodes):
     y = sum([n[1] for n in elmt_nodes]) / len(elmt_nodes)
     z = sum([n[2] for n in elmt_nodes]) / len(elmt_nodes)
     return [x, y, z]
-    
+
+
 def element_to_lines(element, nodes):
     xs = [nodes[nid][0] for nid in element]
     ys = [nodes[nid][1] for nid in element]
@@ -114,8 +120,9 @@ def element_to_lines(element, nodes):
     ys.append(ys[0])
     return (xs, ys)
 
-def write_debug_points_to_shape(all_points):
-    w = shapefile.Writer("./testdata/debug_lines.shp")
+
+def write_debug_points_to_shape(all_points, name="debug"):
+    w = shapefile.Writer("./testdata/{}_lines.shp".format(name))
     w.field('LID', 'N')
 
     for i, points in enumerate(all_points):
@@ -123,7 +130,7 @@ def write_debug_points_to_shape(all_points):
         w.record(i)
     w.close()
 
-    w2 = shapefile.Writer("./testdata/debug_points.shp")
+    w2 = shapefile.Writer("./testdata/{}_points.shp".format(name))
     w2.field('LID')
     w2.field('PID')
 
